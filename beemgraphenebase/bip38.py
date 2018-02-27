a9dfc1138196ed9cf005baeba710d415ea5d18c5
Holger Nahrstaedt <holger@nahrstaedt.de>
2018-02-27 15:32:48 +0100

    next fix

--- beemgraphenebase/bip38.py
+++ beemgraphenebase/bip38.py
@@ -2,7 +2,8 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from builtins import bytes
+from builtins import bytes, int, str
+from builtins import super
 import sys
 import logging
 import hashlib
