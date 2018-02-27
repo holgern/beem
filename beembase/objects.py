1f5b3aeb198e1c19d385aac9d935c93fb534d865
Holger Nahrstaedt <holger@nahrstaedt.de>
2018-02-27 15:35:35 +0100

    next fix

--- beembase/objects.py
+++ beembase/objects.py
@@ -2,8 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-from builtins import str
-from builtins import bytes
+from builtins import bytes, int, str
 from builtins import object
 from builtins import super
 import json
--- beembase/operations.py
+++ beembase/operations.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
+from builtins import bytes, int, str
 from builtins import super
 from collections import OrderedDict
 import json
