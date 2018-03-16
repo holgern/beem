b620e4b496e3065ba9edc0df60248c0a68f049c1
Holger Nahrstaedt <holger@nahrstaedt.de>
2018-03-16 09:32:33 +0100

    Add test_testnet again
    remove secp256k1 from circelei

---
+++ tests/test_testnet.py
@@ -0,0 +1,253 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+from builtins import range
+from builtins import super
+import mock
+import string
+import unittest
+import random
+from pprint import pprint
+from beem import Steem
+from beem.amount import Amount
+from beem.witness import Witness
+from beem.account import Account
+from beem.instance import set_shared_steem_instance
+from beem.blockchain import Blockchain
+from beem.block import Block
+from beemgraphenebase.account import PasswordKey, PrivateKey, PublicKey
+from beem.utils import parse_time, formatTimedelta
+from beemgrapheneapi.rpcutils import NumRetriesReached
+
+# Py3 compatibility
+import sys
+
+password = "yfABsiDXWcCVyDC2udXGYD2psFUiQy"
+core_unit = "STX"
+
+
+class Testcases(unittest.TestCase):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        set_shared_steem_instance(None)
+        self.bts = Steem(
+            node=["wss://testnet.steem.vc"],
+            nobroadcast=True,
+        )
+        # from getpass import getpass
+        # self.bts.wallet.unlock(getpass())
+        set_shared_steem_instance(self.bts)
+        self.bts.set_default_account("beem")
+        stm = self.bts
+        stm.wallet.wipe(True)
+        stm.wallet.create("123")
+        stm.wallet.unlock("123")
+        account_name = "beem"
+        active_key = PasswordKey(account_name, password, role="active", prefix=stm.prefix)
+        owner_key = PasswordKey(account_name, password, role="owner", prefix=stm.prefix)
+        posting_key = PasswordKey(account_name, password, role="posting", prefix=stm.prefix)
+        memo_key = PasswordKey(account_name, password, role="memo", prefix=stm.prefix)
+        active_privkey = active_key.get_private_key()
+        posting_privkey = posting_key.get_private_key()
+        owner_privkey = owner_key.get_private_key()
+        memo_privkey = memo_key.get_private_key()
+        stm.wallet.addPrivateKey(owner_privkey)
+        stm.wallet.addPrivateKey(active_privkey)
+        stm.wallet.addPrivateKey(memo_privkey)
+        stm.wallet.addPrivateKey(posting_privkey)
+
+    def test_transfer(self):
+        bts = self.bts
+        # bts.prefix ="STX"
+        acc = Account("beem", steem_instance=bts)
+        tx = acc.transfer(
+            "test1", 1.33, "SBD", memo="Foobar")
+        self.assertEqual(
+            tx["operations"][0][0],
+            "transfer"
+        )
+        op = tx["operations"][0][1]
+        self.assertIn("memo", op)
+        self.assertEqual(op["from"], "beem")
+        self.assertEqual(op["to"], "test1")
+        amount = Amount(op["amount"])
+        self.assertEqual(float(amount), 1.33)
+
+    def test_create_account(self):
+        bts = self.bts
+        name = ''.join(random.choice(string.ascii_lowercase) for _ in range(12))
+        key1 = PrivateKey()
+        key2 = PrivateKey()
+        key3 = PrivateKey()
+        key4 = PrivateKey()
+        key5 = PrivateKey()
+        tx = bts.create_account(
+            name,
+            creator="beem",
+            owner_key=format(key1.pubkey, core_unit),
+            active_key=format(key2.pubkey, core_unit),
+            posting_key=format(key3.pubkey, core_unit),
+            memo_key=format(key4.pubkey, core_unit),
+            additional_owner_keys=[format(key5.pubkey, core_unit)],
+            additional_active_keys=[format(key5.pubkey, core_unit)],
+            additional_owner_accounts=["test1"],  # 1.2.0
+            additional_active_accounts=["test1"],
+            storekeys=False
+        )
+        self.assertEqual(
+            tx["operations"][0][0],
+            "account_create"
+        )
+        op = tx["operations"][0][1]
+        role = "active"
+        self.assertIn(
+            format(key5.pubkey, core_unit),
+            [x[0] for x in op[role]["key_auths"]])
+        self.assertIn(
+            format(key5.pubkey, core_unit),
+            [x[0] for x in op[role]["key_auths"]])
+        self.assertIn(
+            "test1",
+            [x[0] for x in op[role]["account_auths"]])
+        role = "owner"
+        self.assertIn(
+            format(key5.pubkey, core_unit),
+            [x[0] for x in op[role]["key_auths"]])
+        self.assertIn(
+            format(key5.pubkey, core_unit),
+            [x[0] for x in op[role]["key_auths"]])
+        self.assertIn(
+            "test1",
+            [x[0] for x in op[role]["account_auths"]])
+        self.assertEqual(
+            op["creator"],
+            "beem")
+
+    def test_connect(self):
+        self.bts.connect(node=["wss://testnet.steem.vc"])
+        bts = self.bts
+        self.assertEqual(bts.prefix, "STX")
+
+    def test_set_default_account(self):
+        self.bts.set_default_account("beem")
+
+    def test_info(self):
+        info = self.bts.info()
+        for key in ['current_witness',
+                    'head_block_id',
+                    'head_block_number',
+                    'id',
+                    'last_irreversible_block_num',
+                    'current_witness',
+                    'total_pow',
+                    'time']:
+            self.assertTrue(key in info)
+
+    def test_finalizeOps(self):
+        bts = self.bts
+        tx1 = bts.new_tx()
+        tx2 = bts.new_tx()
+
+        acc = Account("beem", steem_instance=bts)
+        acc.transfer("test1", 1, "STEEM", append_to=tx1)
+        acc.transfer("test1", 2, "STEEM", append_to=tx2)
+        acc.transfer("test1", 3, "STEEM", append_to=tx1)
+        tx1 = tx1.json()
+        tx2 = tx2.json()
+        ops1 = tx1["operations"]
+        ops2 = tx2["operations"]
+        self.assertEqual(len(ops1), 2)
+        self.assertEqual(len(ops2), 1)
+
+    def test_weight_threshold(self):
+        bts = self.bts
+        auth = {'account_auths': [['test', 1]],
+                'extensions': [],
+                'key_auths': [
+                    ['STX55VCzsb47NZwWe5F3qyQKedX9iHBHMVVFSc96PDvV7wuj7W86n', 1],
+                    ['STX7GM9YXcsoAJAgKbqW2oVj7bnNXFNL4pk9NugqKWPmuhoEDbkDv', 1]],
+                'weight_threshold': 3}  # threshold fine
+        bts._test_weights_treshold(auth)
+        auth = {'account_auths': [['test', 1]],
+                'extensions': [],
+                'key_auths': [
+                    ['STX55VCzsb47NZwWe5F3qyQKedX9iHBHMVVFSc96PDvV7wuj7W86n', 1],
+                    ['STX7GM9YXcsoAJAgKbqW2oVj7bnNXFNL4pk9NugqKWPmuhoEDbkDv', 1]],
+                'weight_threshold': 4}  # too high
+
+        with self.assertRaises(ValueError):
+            bts._test_weights_treshold(auth)
+
+    def test_allow(self):
+        bts = self.bts
+        self.assertIn(bts.prefix, "STX")
+        acc = Account("beem", steem_instance=bts)
+        self.assertIn(acc.steem.prefix, "STX")
+        tx = acc.allow(
+            "STX55VCzsb47NZwWe5F3qyQKedX9iHBHMVVFSc96PDvV7wuj7W86n",
+            account="beem",
+            weight=1,
+            threshold=1,
+            permission="owner",
+        )
+        self.assertEqual(
+            (tx["operations"][0][0]),
+            "account_update"
+        )
+        op = tx["operations"][0][1]
+        self.assertIn("owner", op)
+        self.assertIn(
+            ["STX55VCzsb47NZwWe5F3qyQKedX9iHBHMVVFSc96PDvV7wuj7W86n", '1'],
+            op["owner"]["key_auths"])
+        self.assertEqual(op["owner"]["weight_threshold"], 1)
+
+    def test_disallow(self):
+        bts = self.bts
+        acc = Account("beem", steem_instance=bts)
+        if sys.version > '3':
+            _assertRaisesRegex = self.assertRaisesRegex
+        else:
+            _assertRaisesRegex = self.assertRaisesRegexp
+        with _assertRaisesRegex(ValueError, ".*Changes nothing.*"):
+            acc.disallow(
+                "STX55VCzsb47NZwWe5F3qyQKedX9iHBHMVVFSc96PDvV7wuj7W86n",
+                weight=1,
+                threshold=1,
+                permission="owner"
+            )
+        with _assertRaisesRegex(ValueError, ".*Changes nothing!.*"):
+            acc.disallow(
+                "STX6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV",
+                weight=1,
+                threshold=1,
+                permission="owner"
+            )
+
+    def test_update_memo_key(self):
+        bts = self.bts
+        self.assertEqual(bts.prefix, "STX")
+        acc = Account("beem", steem_instance=bts)
+        tx = acc.update_memo_key("STX55VCzsb47NZwWe5F3qyQKedX9iHBHMVVFSc96PDvV7wuj7W86n")
+        self.assertEqual(
+            (tx["operations"][0][0]),
+            "account_update"
+        )
+        op = tx["operations"][0][1]
+        self.assertEqual(
+            op["memo_key"],
+            "STX55VCzsb47NZwWe5F3qyQKedX9iHBHMVVFSc96PDvV7wuj7W86n")
+
+    def test_approvewitness(self):
+        bts = self.bts
+        w = Account("beem", steem_instance=bts)
+        tx = w.approvewitness("test1")
+        self.assertEqual(
+            (tx["operations"][0][0]),
+            "account_witness_vote"
+        )
+        op = tx["operations"][0][1]
+        self.assertIn(
+            "test1",
+            op["witness"])
